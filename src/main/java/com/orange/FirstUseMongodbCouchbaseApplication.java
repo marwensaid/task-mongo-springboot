f8b7cd44389c1fbbaf55570f3ced79dc0ddf1fd3
marwensaid <marwensaidi1@gmail.com>
2015-12-17 13:47:18 +0100

    feat(task mongo) add project struct --MS

---
+++ src/main/java/com/orange/FirstUseMongodbCouchbaseApplication.java
@@ -0,0 +1,12 @@
+package com.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstUseMongodbCouchbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstUseMongodbCouchbaseApplication.class, args);
+    }
+}
